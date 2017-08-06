b10070d8c52cd0cc0c0dedbafa9cf71011a9086c
RickCole21 <rickcole21@outlook.com>
2017-08-06 18:29:26 +0800

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,11 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h2>欢迎来到Rick's World</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          想要开始, 编辑 <code>src/App.js</code> 然后保存后重载.
         </p>
       </div>
     );
