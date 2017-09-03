e4af1b7b16bd8a1da65370290165bb72cb33482b
RickCole21 <rickcole21@outlook.com>
2017-09-03 13:37:58 +0800

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from "./component/Header";
+import Clock from "./component/Clock";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header/>
+        <Clock/>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo"/>
           <h2>欢迎来到Rick's World</h2>
