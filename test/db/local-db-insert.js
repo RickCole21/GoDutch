57e15b3a225acc1fe1fe1b8705329f1c4427dd25
RickCole21 <rickcole21@outlook.com>
2017-09-05 22:06:29 +0800

    init

--- test/db/local-db-insert.js
+++ test/db/local-db-insert.js
@@ -6,14 +6,45 @@ var insertData = function (db, callback) {
   //连接到表 site
   var collection = db.collection('site');
   //插入数据
-  var data = [
-    {
-      "name": "菜鸟教程", "url": "www.runoob.com"
-    },
-    {
-      "name": "菜鸟工具", "url": "c.runoob.com"
-    }
-  ];
+  var data = [{
+    "name": "aaa", "url": "www.aaa.com"
+  }, {
+    "name": "bbb", "url": "www.bbb.com"
+  }, {
+    "name": "ccc", "url": "www.ccc.com"
+  }, {
+    "name": "ddd", "url": "www.ddd.com"
+  }, {
+    "name": "eee", "url": "www.eee.com"
+  }, {
+    "name": "fff", "url": "www.fff.com"
+  }, {
+    "name": "ggg", "url": "www.ggg.com"
+  }, {
+    "name": "hhh", "url": "www.hhh.com"
+  }, {
+    "name": "iii", "url": "www.iii.com"
+  }, {
+    "name": "jjj", "url": "www.jjj.com"
+  }, {
+    "name": "kkk", "url": "www.kkk.com"
+  }, {
+    "name": "lll", "url": "www.lll.com"
+  }, {
+    "name": "mmm", "url": "www.mmm.com"
+  }, {
+    "name": "nnn", "url": "www.nnn.com"
+  }, {
+    "name": "ooo", "url": "www.ooo.com"
+  }, {
+    "name": "ppp", "url": "www.ppp.com"
+  }, {
+    "name": "qqq", "url": "www.qqq.com"
+  }, {
+    "name": "rrr", "url": "www.rrr.com"
+  }, {
+    "name": "sss", "url": "www.sss.com"
+  }];
   collection.insert(data, function (err, result) {
     if (err) {
       console.log('Error:' + err);
