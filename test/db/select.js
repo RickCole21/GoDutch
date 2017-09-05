8ca938a8c6bdc3d4466bfd35e51af6aaeed7fc31
RickCole21 <rickcole21@outlook.com>
2017-09-05 22:25:31 +0800

    init

--- test/db/select.js
+++ test/db/select.js
@@ -1,11 +1,15 @@
+// 本地测试时，需要先打开 mongodb : cd Documents --> mongod --dbpath data/db
 var MongoClient = require('mongodb').MongoClient;
-var DB_CONN_STR = 'mongodb://RickCole:.dante2226120@getstarted-shard-00-00-ambre.mongodb.net:27017,getstarted-shard-00-01-ambre.mongodb.net:27017,getstarted-shard-00-02-ambre.mongodb.net:27017/ricktest?ssl=true&replicaSet=GetStarted-shard-0&authSource=admin';
+var DB_CONN_STR = 'mongodb://localhost:27017/test';
 
 var selectData = function (db, callback) {
   //连接到表
   var collection = db.collection('site');
   //查询数据
-  var whereStr = {"name": 'rick'};
+  var whereStr = {
+    "username": 'rickcole'
+  };
+  
   collection.find(whereStr).toArray(function (err, result) {
     if (err) {
       console.log('Error:' + err);
